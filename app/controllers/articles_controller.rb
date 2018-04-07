07d3952bf59f499bf2bdd9a007076cf7c9f05365
vkushnarenko <kote.godue@gmail.com>
2018-04-07 22:21:12 +0000

    Add,Edit,Update,Index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 
+def index
+    @articles= Article.all
+end
 
 def new
     @article = Article.new
@@ -20,6 +23,21 @@ def show
  @article = Article.find(params[:id])    
 end 
 
+def edit
+     @article = Article.find(params[:id])  
+end    
+
+def update
+    #render plain: params[:article].inspect 
+  @article = Article.find(params[:id])  
+   if @article.update(article_params)
+       flash[:notice] = "Successfully updated"
+   redirect_to article_path(@article)
+   else        
+   render 'new'   
+   end  
+end
+    
 private
 def article_params
     params.require(:article).permit(:title, :description)
