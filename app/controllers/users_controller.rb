01e16e3b9ab83ece22a16332680360ef5e0fb77d
vkushnarenko <kote.godue@gmail.com>
2018-10-23 21:10:29 +0000

    hotfix for user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if user.save
+    if  @user.save
       flash[:success] = "Welcome to Alpha Blog #{@user.username}"
-      redirect_to articles_path
+     redirect_to articles_path
     else
       render 'new'
     end
